9dd520233b3bee315d1dbf25b3d01b7c795b1731
sk-upadhyay <shivamws2003@gmail.com>
2024-02-02 15:43:43 +0530

    modified:   src/viz.js

--- src/viz.js
+++ src/viz.js
@@ -1,9 +1,9 @@
 import * as vl from 'vega-lite-api';
 
 export const viz = vl
-  .markBar()
+  .markCircle({stroke : false, size:20})
   .encode(
-    vl.x().fieldQ('temperature').scale({ zero: false }),
+    vl.x().fieldT('timestamp').scale({ zero: false }),
     vl.y().fieldQ('pm10').scale({ zero: false }),
     vl.tooltip('device_id')
   );
